e64759f86097a24c59dd5cf9b23043867ea8e3f6
Nishchaya Narula <58116972+nishchaya2k@users.noreply.github.com>
2024-09-22 16:07:44 +0530

    Add files via upload

---
+++ ToggleButton2/script.js
@@ -0,0 +1,6 @@
+const wrapper = document.querySelector('.wrapper')
+const circle = document.querySelector('.circle')
+
+wrapper.addEventListener('click', () => {
+    wrapper.classList.toggle('active')
+})
