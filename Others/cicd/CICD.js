be2bbee106af212bc5d48523911cfa2ac5c65a01
Nishchaya Narula <n.nishchaya2000@gmail.com>
2025-08-10 03:07:50 +0530

    CICD;2C

---
+++ Others/cicd/CICD.js
@@ -0,0 +1,54 @@
+/*
+- CICD
+
+1. Its a way to make software faster, safer & efficient by streamlining and automating process like building, testing & delivering of software
+
+2. Continuous Integration,Continuous Delivery, & also Continuous deployment is a process to automate software delivery by automating tasks like build,test & deploy
+
+- Continuous Integration 
+
+1. Developers frequently merge code into a shared branch 
+2. Each merge triggers an automated pipeline that:
+    Builds the code
+    Runs tests
+    Checks for errors/lint
+3. Goal: Catch bugs early and ensure integration doesn’t break the app.
+
+
+- Continuous Delivery (CD):
+
+1. After passing tests, the code is packaged (e.g., into a Docker image).
+2. It is automatically delivered to a staging or testing environment.
+3. Goal: Keep the app always ready for release.
+
+
+- Continuous Deployment (CD – optional):
+
+1. Goes a step further than delivery.
+2. After all tests pass, the new code is automatically deployed to production.
+3. Goal: Zero-touch, safe, rapid releases.
+
+
+
+- Benefits of Using CICD:
+
+1. Quicker time to market: as automate the process and reduce the manuall intervention, which allows us to deploy application faster.
+
+2. Higher Software Quality: automated testing helps us to find bugs and issues early in the development process
+
+3. Reduced Risk: Can push smaller changes and get more quicker updates which are less likely to cause major problems or bring production down
+
+4. Collaboration: Using CICD, developers, testers, & operation team can collaborate effectively
+
+5. Continous Improvement: Feedbacks that you get during CICD stages, can be beneficial
+
+
+- CICD Tools in Devops
+
+1. Bamboo, circleci, spinnaker, pagerduty, jenkins, travis ci, jira, heroku, github, aws code pipeline, team city, go(continuos delivery)
+
+
+
+- Docker Image
+
+*/
