7739f04ec783ff15feec30d327da92274886ae3e
Nishchaya Narula <n.nishchaya2000@gmail.com>
2025-08-28 00:29:56 +0530

    dsa

---
+++ Others/realTimeUpdates/ShortPolling.js
@@ -0,0 +1,8 @@
+/*
+- Short Polling: 
+
+1. In short polling, the client repeatedly sends requests to the server at regular intervals to check for updates or new data.
+
+2. The server responds to these requests immediately, even if there is no new information available. This process continues at regular intervals, with the client periodically polling the server for updates.
+
+*/
