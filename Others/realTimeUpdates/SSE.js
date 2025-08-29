61489c1d0b86657b8c9ed69df555e82e7196ea29
Nishchaya Narula <n.nishchaya2000@gmail.com>
2025-08-29 09:26:21 +0530

    dsa

--- Others/realTimeUpdates/SSE.js
+++ Others/realTimeUpdates/SSE.js
@@ -1,5 +1,11 @@
 /*
 - SSE
 
-1. 
+1. In Server-Sent Events (SSE), the client establishes a persistent unidirectional connection with the server using a standard HTTP request.
+
+2. The server sends events or updates to the client as they occur.
+
+3. The connection remains open, and the server can keep sending events to the client without the need for the client to initiate additional requests. This allows for real-time communication from the server to the client.
+
+eg. Chat GPT
 */
