a5d72a3e845e847fb2bc60418d731f3e21ae45d6
Nishchaya Narula <n.nishchaya2000@gmail.com>
2025-09-03 09:38:43 +0530

    dsa

--- DSA/String/2_LongestCommonPrefix.js
+++ DSA/String/2_LongestCommonPrefix.js
@@ -4,21 +4,51 @@ Write a function to find the longest common prefix string amongst an array of st
 If there is no common prefix, return an empty string "".
 */
 
-let str = ["flowers", "flow", "fly", "flight"]
+let str = ["flowers", "flow", "fly", "ight"]
 
 
-// Approach 1, Brute Force
-function lcp(str) {
+// Approach 1, Brute Force, TC: O(n2)
+function lcp1(str) {
 
     if (str?.length == 0) return '';
-    let str = "";
+    let lcp = 0;
+    let j = 1;
 
 
     for (let i = 0; i < str[0].length; i++) {
-        for (let j = 1; j < str[i].length; i++) {
-            
+        for (j = 1; j < str.length; j++) {
+            if (str[0][i] != str[j][i]) break;
+        }
+
+        if (j == str.length) {
+            lcp++;
+        } else {
+            break;
         }
     }
+    return str[0].slice(0, lcp);
 }
 
-console.log("Longest Common Prefix", str)
+console.log("Longest Common Prefix", lcp1(str))
+
+
+//Approach 2, Brute Force 
+function lcp2(str) {
+
+    if (str?.length == 0) return '';
+    let lcp = 0;
+    let j = 1;
+
+
+    for (let i = 0; i < str[0].length; i++) {
+        for (j = 1; j < str.length; j++) {
+            if (str[0][i] != str[j][i]) {
+                return str[0].slice(0, lcp)
+            }
+        }
+        lcp++;
+    }
+    return str[0].slice(0, lcp);
+}
+
+console.log("Longest Common Prefix", lcp2(str))
---
+++ Others/networkProtocols/NetworkProtocols.js
@@ -0,0 +1,9 @@
+/*
+- Network protocols are a set of rules to follow when two devices communicate with each other. It’s like a common language that both devices and systems understand.
+
+
+https://medium.com/@rano3003/system-design-fundamentals-network-protocols-c0c8925f4c1f
+*/
+
+
+//Doubt: Not able to visualize how things are happing behind the scene
