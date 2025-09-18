6cd11dd5f4f5c7329b2204599ee0ce626d3dd274
Nishchaya Narula <n.nishchaya2000@gmail.com>
2025-09-18 09:31:55 +0530

    dsa

---
+++ Others/httpHeaders/HttpHeaders.js
@@ -0,0 +1,7 @@
+/*
+- 🔹 HTTP Headers: They are an important part of api request and response as they represent meta-data associated with the api request and response.
+Headers carry information for the request and response body
+
+- 🔹 
+
+*/
