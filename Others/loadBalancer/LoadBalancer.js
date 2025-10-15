37c4c374d549ef98ed6ba36dc609e8c4ef9f4269
Nishchaya Narula <n.nishchaya2000@gmail.com>
2025-10-16 00:22:30 +0530

    fix: loadBalancer

---
+++ Others/loadBalancer/LoadBalancer.js
@@ -0,0 +1,16 @@
+/*
+- Load Balancer: A load balancer is a system that distributes incoming network traffic (like user requests) across multiple servers, so no single server gets overloaded.
+
+eg. A traffic cop standing at a highway fork, directing cars evenly to each road so that none gets jammed.
+
+
+- Why we need it
+
+1. Prevents one server from crashing under heavy load.
+2. Improves reliability — if one server fails, others keep working.
+3. Increases scalability — you can add more servers behind the load balancer as user traffic grows.
+4. Helps with geographic distribution — send users to the nearest server region.
+
+
+
+*/
