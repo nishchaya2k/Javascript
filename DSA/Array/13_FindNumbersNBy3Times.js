4865ba591aa718097539bdad89b0adeb995364db
Nishchaya Narula <n.nishchaya2000@gmail.com>
2025-11-16 07:55:43 +0530

    dsa

--- DSA/Array/13_FindNumbersNBy3Times.js
+++ DSA/Array/13_FindNumbersNBy3Times.js
@@ -29,16 +29,54 @@ function findNoGreaterThenNBy3_1(arr) {
 console.log("Find Number", findNoGreaterThenNBy3_1(arr))
 
 
-//Approach 2
+//Approach 2, Moore Voting Algo
 function findNoGreaterThenNBy3_2(arr) {
+    let count1 = 0;
+    let count2 = 0;
+    let el1 = null;
+    let el2 = null;
+    let n = arr.length;
+    let result = [];
 
+    for (let i = 0; i < n; i++) {
+        if (count1 == 0 && el2 !== arr[i]) {
+            el1 = arr[i];
+            count1 = 1;
+        }
+        else if (count2 == 0 && el1 !== arr[i]) {
+            el2 = arr[i];
+            count2 = 1;
+        } else if (el1 == arr[i]) {
+            count1++;
+        }
+        else if (el2 == arr[i]) {
+            count2++;
+        } else {
+            count1--;
+            count2--;
+        }
+    }
+
+    count1 = 0;
+    count2 = 1;
+
+    for (let num of arr) {
+        if (num === el1) count1++;
+        else if (num === el2) count2++;
+    }
+
+
+    if (count1 > (n / 3)) result.push(el1)
+    if (count2 > (n / 3)) result.push(el2)
+
+    return result;
 }
+
+
 
 console.log("Find Number", findNoGreaterThenNBy3_2(arr))
 
-//Approach 3
-function findNoGreaterThenNBy3_3(arr) {
 
-}
-
-console.log("Find Number", findNoGreaterThenNBy3_3(arr))
+/*
+Note: Note More than 2 Numbers can be solution.
+*/
