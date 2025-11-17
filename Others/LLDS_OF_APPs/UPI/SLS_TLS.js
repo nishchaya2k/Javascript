1af68e49fe2f4a60b37aef196cc7b9a092ddcc83
Nishchaya Narula <n.nishchaya2000@gmail.com>
2025-11-17 07:13:25 +0530

    hld_UPI

---
+++ Others/LLDS_OF_APPs/UPI/SLS_TLS.js
@@ -0,0 +1,28 @@
+/*
+
+- 🔹 SLS/TLS: Secure Socket Layer/Tranport Layer Security
+
+  Its a Cryptographic protocols designed to provide secure communication over a computer network
+
+- 🔹 TLS successor to SLS
+
+- 🔹 How SLS/TLS secure the data
+
+1.  Encrypt Data & Ensures its
+  - Integrity: Accessed only by Particular Entity
+  - Confidentiality: Not Modified in between
+  - Authenticity: Verifying the identity of the parties who they are supposed to be
+  between client and server
+  - https://www.youtube.com/watch?v=I43NnHTeACk
+
+
+
+
+- 🔹 Certificates
+
+1.  A certificate is a digital document that verifies the identity of a server, websites
+2.  Certificates contain the server's public key from provided by Authority
+3.  Browser have already details of websites so it verifies from certificate fingerprint and public key
+
+
+*/
---
+++ Others/LLDS_OF_APPs/UPI/UPI1.js
@@ -0,0 +1,39 @@
+/*
+Designing a Unified Payments Interface (UPI) system involves creating a robust and secure architecture that enables real-time inter-bank transactions. UPI, managed by the National Payments Corporation of India (NPCI), is a platform that allows seamless fund transfers between bank accounts through mobile devices.
+
+- 🔹 NPCI: NPCI provides the ecosystem for routing, processing and settlement services to members participating in UPI. NPCI also performs approval of participating PSPs, conducting audits of the PSPs, report generation etc.
+
+- 🔹 PCP: PSP (Payment service Provider) is one of the entities in UPI ecosystem whose responsibility is to provide users with a frontend application which can be used for generating the VPAs and carrying out transactions.
+
+- 🔹 User Interfaces:
+
+1.  UPI apps are developed by banks and third-party service providers. These mobile applications allow users to initiate and manage transactions, view history, and interact with payment services.
+
+2.  Managed by NPCI (Infrastruture), the central switch acts as the intermediary that routes transaction requests between banks, ensuring proper authentication, authorization, and secure fund transfer.
+
+3.  Each participating bank’s backend system interacts with the UPI switch to process and approve transactions. These systems ensure that money is transferred from the sender’s account to the receiver’s account in real time.
+
+4.  A Virtual Payment Address (VPA) is a unique identifier that helps UPI to track a person’s account. It acts as an ID independent of your bank account number and other details. VPA can be used to make and request payments through a UPI-enabled app. You need not fill in your bank account details repeatedly for making multiple payments.
+
+5.  Users register on the platform by linking their bank accounts using their mobile numbers. Multi-factor authentication (e.g., mobile number OTP, MPIN) ensures that only authorized users can access their accounts.
+
+
+6. Payments are initiated using a UPI ID or Virtual Payment Address (VPA), a unique identifier linked to the user’s bank account. Users can also transfer funds using the receiver’s mobile number or by scanning QR codes.
+
+
+7. Once a payment is authorized, the UPI switch routes the transaction request to the receiver’s bank for approval and settlement. The entire process happens in real time, ensuring seamless and instant transfers.
+
+
+8.  UPI enables immediate settlement of funds between banks, reducing the complexity and delays associated with traditional banking transactions.
+
+
+
+
+
+
+
+
+ 
+
+
+*/
