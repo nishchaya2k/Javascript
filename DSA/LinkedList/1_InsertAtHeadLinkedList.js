42f812d738651b60130488f8d3001b9ce2afa1dc
Nishchaya Narula <n.nishchaya2000@gmail.com>
2026-01-17 23:49:16 +0530

    Feat: add dsa solution for "2D Insertion in Linked List"

--- DSA/LinkedList/1_InsertAtHeadLinkedList.js
+++ DSA/LinkedList/1_InsertAtHeadLinkedList.js
@@ -1,5 +1,5 @@
 /*
-Insert at the head of a Linked List
+Insert at the head of a Linked List for 1D and 2D Array
 
 Problem Statement: Given a linked list and an integer value val, insert a new node with that value at the beginning (before the head) of the list and return the updated linked list.
 */
@@ -8,6 +8,14 @@ Problem Statement: Given a linked list and an integer value val, insert a new no
 class Node {
     constructor(data, next = null) {
         this.data = data;
+        this.next = next;
+    }
+}
+
+class Node2D {
+    constructor(data, child = null, next = null) {
+        this.data = data;
+        this.child = child;
         this.next = next;
     }
 }
@@ -37,6 +45,47 @@ function llCreate(arr) {
 
     return head;
 }
-module.exports = { llCreate };
+
+function llCreate2D(arr) {
+    if (arr.length === 0) return null;
+    let curr = null;
+    let head = null;
+
+    for (let i = 0; i < arr.length; i++) {
+        let currChild = null;
+        for (j = 0; j < arr[i].length; j++) {
+            if (i == 0 && j == 0) {
+                curr = new Node2D(arr[i][j])
+                head = curr
+                currChild = curr
+            } else if (j == 0) {
+                curr.next = new Node2D(arr[i][j])
+                curr = curr.next;
+                currChild = curr
+            } else {
+                currChild.child = new Node2D(arr[i][j])
+                currChild = currChild.child
+            }
+        }
+    }
+    // Print the linked list
+    // let temp = head;
+
+    // while (temp) {
+    //     let curr = temp;
+    //     while (curr) {
+    //         console.log(curr.data, "|")
+    //         curr = curr.child
+    //     }
+    //     console.log("->")
+    //     temp = temp.next;
+    // }
+
+    return head;
+}
+
+module.exports = { llCreate, llCreate2D };
+
 // Example:
 // llCreate([1, 2, 3, 4, 2, 5]);
+// llCreate2D([[3], [2, 10], [1, 7, 11, 12], [4, 9], [5, 6, 8]]);
